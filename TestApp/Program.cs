5f60f5e455956d9c096dc4a5076479c6def2a9ad
Bjoern Stoffers <bjoern@stoffix.de>
2019-03-03 14:23:10 +0100

    34

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World3!");
+            Console.WriteLine("Hello World34!");
         }
     }
 }
